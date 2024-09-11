e5bb3705d8f619d52a17273d4c903aa7ca980c9e
Adam YD <adamyd18@gmail.com>
2024-09-11 12:26:29 +0700

    Update miku-jmbd.sh

--- miku-jmbd.sh
+++ miku-jmbd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 rm -rf .repo/local_manifests/
-rm -rf .repo
 rm -rf device/lineage/sepolicy
-repo init -u https://github.com/Miku-UI/manifesto -b Udon_v2
+rm -rf vendor/lineage
 git clone https://github.com/XeroMz69/manifest-lokal.git -b miku .repo/local_manifests
+repo init -u https://github.com/Miku-UI/manifesto -b Udon_v2
 /opt/crave/resync.sh
 export BUILD_USERNAME=Xero
 export BUILD_HOSTNAME=crave
@@ -12,6 +12,7 @@ export KBUILD_USERNAME=Xero
 export KBUILD_HOSTNAME=crave
 export TZ=Asia/Jakarta
 source build/envsetup.sh
+. build/envsetup.sh
 lunch miku_earth-userdebug-UNOFFICIAL
 make installclean
 make diva
