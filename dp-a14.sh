e4c4188336285427db3b474c4f2ce7e994cc5686
Adam YD <adamyd18@gmail.com>
2024-10-02 06:56:58 +0700

    Update and rename dp-a15.sh to dp-a14.sh

--- dp-a14.sh
+++ dp-a14.sh
@@ -8,11 +8,6 @@ rm -rf device/xiaomi/earth
 rm -rf kernel/xiaomi/earth
 rm -rf vendor/xiaomi/earth
 rm -rf hardware/xiaomi
-rm -rf prebuilts/gcc/linux-x86/aarch64/aarch64-elf
-rm -rf prebuilts/gcc/linux-x86/arm/arm-eabi
-rm -rf prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
-rm -rf prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.9
-rm -rf prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9
 echo "==============removing previous tree done=========="
 # Initialize repo
 git clone https://github.com/XeroMz69/manifest-lokal -b derpfest .repo/local_manifests --depth=1
@@ -21,9 +16,13 @@ echo "================repo Initialize success========="
 # repo sync
 /opt/crave/resync.sh
 echo "==========repo sync success====="
-# remove source frameworks/base
-rm -rf frameworks/base
-echo "============remove frameworks/base success============" 
+# remove clang from source
+rm -rf prebuilts/gcc/linux-x86/aarch64/aarch64-elf
+rm -rf prebuilts/gcc/linux-x86/arm/arm-eabi
+rm -rf prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
+rm -rf prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.9
+rm -rf prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9
+echo "============remove clang success============" 
 # hardware tree
 git clone https://github.com/yaap/hardware_xiaomi.git -b fourteen hardware/xiaomi --depth=1
 # prebuilts
@@ -32,8 +31,6 @@ git clone https://github.com/StatiXOS/android_prebuilts_gcc_linux-x86_arm_arm-ea
 git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git -b lineage-19.1 prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 --depth=1
 git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_x86_x86_64-linux-android-4.9.git -b lineage-19.1 prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.9 --depth=1
 git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git -b lineage-19.1 prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 --depth=1
-# source modification
-git clone https://github.com/DerpFest-AOSP/frameworks_base.git -b 14 frameworks/base --depth=1
 # set build environment
 . build/envsetup.sh
 export BUILD_USERNAME=Xero
