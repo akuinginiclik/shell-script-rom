371639c5e93ea1fde2af815aed46466d7be25621
Adam YD <adamyd18@gmail.com>
2024-11-23 18:14:29 +0700

    Update derp.sh

--- derp.sh
+++ derp.sh
@@ -4,14 +4,12 @@ rm -rf device/lineage/sepolicy
 repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 15
 git clone https://github.com/XeroMz69/manifest-lokal.git -b derpfest .repo/local_manifests
 /opt/crave/resync.sh
-export BUILD_USERNAME=Xero
-export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export KBUILD_USERNAME=Xero
 export KBUILD_HOSTNAME=crave
 export TZ=Asia/Jakarta
 source build/envsetup.sh
 . build/envsetup.sh
-lunch derp_earth-user
+lunch derp_earth-userdebug
 make installclean
 mka derp
