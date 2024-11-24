c09cb8232aaea4e68be9fbfbf468da881a2c51bf
Adam YD <adamyd18@gmail.com>
2024-11-24 14:56:27 +0700

    Update derp.sh

--- derp.sh
+++ derp.sh
@@ -4,6 +4,8 @@ rm -rf device/lineage/sepolicy
 repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 15
 git clone https://github.com/XeroMz69/manifest-lokal.git -b derpfest .repo/local_manifests
 /opt/crave/resync.sh
+rm -rf vendor/xiaomi
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git vendor/xiaomi/earth --depth 1
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export KBUILD_USERNAME=Xero
 export KBUILD_HOSTNAME=crave
