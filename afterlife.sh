6a475480800ac01ecf548dcbafc1fb3c7a2b6e96
Adam YD <adamyd18@gmail.com>
2024-11-27 10:53:31 +0700

    Update afterlife.sh

--- afterlife.sh
+++ afterlife.sh
@@ -6,7 +6,7 @@ rm -rf vendor/lineage
 repo init --depth=1 -u https://github.com/AfterlifeOS/android_manifest.git -b 14 --git-lfs
 git clone https://github.com/XeroMz69/manifest-lokal.git -b afterlife .repo/local_manifests
 /opt/crave/resync.sh
-git clone https://github.com/LineageOS/android_prebuilts_clang_kernel_linux-x86_clang-r416183b.git prebuilts/prebuilts/clang/host/linux-x86/clang-los
+#git clone https://github.com/LineageOS/android_prebuilts_clang_kernel_linux-x86_clang-r416183b.git prebuilts/prebuilts/clang/host/linux-x86/clang-los
 # rm -rf hardware/qcom hardware/qcom-caf vendor/qcom
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export KBUILD_USERNAME=Xero
