f50b6e4f216b79db9c143d54e6b405d3c1fef02e
Adam YD <adamyd18@gmail.com>
2025-01-21 07:18:28 +0700

    Update derp.sh

--- derp.sh
+++ derp.sh
@@ -3,7 +3,7 @@ rm -rf .repo/local_manifests/
 rm -rf device/lineage/sepolicy
 repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 15
 git clone https://github.com/XeroMz69/manifest-lokal.git -b derpfest .repo/local_manifests
-/opt/crave/resync.sh
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 rm -rf vendor/xiaomi
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git vendor/xiaomi/earth --depth 1
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
