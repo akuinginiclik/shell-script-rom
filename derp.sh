94675c32a031f163203a97dcb8d1296d4412c098
Adam YD <adamyd18@gmail.com>
2025-01-21 20:31:26 +0700

    Update derp.sh

--- derp.sh
+++ derp.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
+
+WORKDIR="$(pwd)"
+
+# Remove some old shit
 rm -rf .repo/local_manifests/
 rm -rf device/lineage/sepolicy
+
+# Syncing the DerpFest repo
 repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 15
 git clone https://github.com/XeroMz69/manifest-lokal.git -b derpfest .repo/local_manifests
 /opt/crave/resync.sh
+
+# Some fixes
 rm -rf vendor/xiaomi
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git vendor/xiaomi/earth --depth 1
 git clone https://github.com/LineageOS/android_prebuilts_clang_kernel_linux-x86_clang-r416183b prebuilts/prebuilts/clang/host/linux-x86/clang-los --depth 1
+
+# Some flags
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export KBUILD_USERNAME=xero
 export KBUILD_HOSTNAME=crave
 export TZ=Asia/Jakarta
+
+# Setup the environment
 . build/envsetup.sh
+
+# Derp it
 lunch derp_earth-userdebug
 make installclean
 mka derp
