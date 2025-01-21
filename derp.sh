81745858aadccad86e185c11b3295e9aea6f1ff2
Adam YD <adamyd18@gmail.com>
2025-01-21 19:27:47 +0700

    Update derp.sh

--- derp.sh
+++ derp.sh
@@ -7,8 +7,8 @@ git clone https://github.com/XeroMz69/manifest-lokal.git -b derpfest .repo/local
 rm -rf vendor/xiaomi
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git vendor/xiaomi/earth --depth 1
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export KBUILD_USERNAME=Xero
-export KBUILD_HOSTNAME=urmom
+export KBUILD_USERNAME=xero
+export KBUILD_HOSTNAME=crave
 export TZ=Asia/Jakarta
 . build/envsetup.sh
 lunch derp_earth-userdebug
