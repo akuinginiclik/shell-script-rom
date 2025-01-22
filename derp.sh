2ebdb8efb2d20f89f8759807381284fccfc2763a
Adam YD <adamyd18@gmail.com>
2025-01-22 16:49:49 +0700

    Update derp.sh

--- derp.sh
+++ derp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+CLANG_DLINK="https://github.com/XeroMz69/Clang/releases/download/Xero-Clang-20.0.0git-20250113/Xero-Clang-20.0.0git-20250113.tar.gz"
 WORKDIR="$(pwd)"
 
 # Remove some old shit
@@ -14,7 +15,13 @@ git clone https://github.com/XeroMz69/manifest-lokal.git -b derpfest .repo/local
 # Some fixes
 rm -rf vendor/xiaomi
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git $WORKDIR/vendor/xiaomi/earth --depth 1
-git clone https://github.com/LineageOS/android_prebuilts_clang_kernel_linux-x86_clang-r416183b $WORKDIR/prebuilts/prebuilts/clang/host/linux-x86/clang-los --depth 1
+# git clone https://github.com/LineageOS/android_prebuilts_clang_kernel_linux-x86_clang-r416183b $WORKDIR/prebuilts/prebuilts/clang/host/linux-x86/clang-los --depth 1
+rm -rf $WORKDIR/prebuilts/prebuilts/clang/host/linux-x86/clang-los
+mkdir -p $WORKDIR/prebuilts/prebuilts/clang/host/linux-x86/clang-los
+sudo apt install aria2 -y
+aria2c -s16 -x16 -k1M $CLANG_DLINK -o Clang.tar.gz
+tar -C $WORKDIR/prebuilts/prebuilts/clang/host/linux-x86/clang-los/ -zxvf Clang.tar.gz
+rm -rf Clang.tar.gz
 
 # Some flags
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
