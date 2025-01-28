b1937b5d3638d517a37374523e8db3f57058d7ba
Adam YD <adamyd18@gmail.com>
2025-01-28 10:49:03 +0700

    Update circle-after.sh

--- circle-after.sh
+++ circle-after.sh
@@ -27,8 +27,8 @@ git-lfs install
 git clone https://github.com/XeroMz69/manifest-lokal.git -b afterlife .repo/local_manifests
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 rm -rf vendor/xiaomi
-git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git $WORKDIR/vendor/xiaomi/earth --depth 1
-git clone https://github.com/LineageOS/android_prebuilts_clang_kernel_linux-x86_clang-r416183b $WORKDIR/prebuilts/prebuilts/clang/host/linux-x86/clang-los --depth 1
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git vendor/xiaomi/earth --depth 1
+git clone https://github.com/LineageOS/android_prebuilts_clang_kernel_linux-x86_clang-r416183b prebuilts/clang/host/linux-x86/clang-xero --depth 1
       
 #  Build_script:
 echo "build"
@@ -40,3 +40,12 @@ export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Jakarta
 . build/envsetup.sh
 goafterlife earth 
+
+# Upload Build
+mkdir -p /tmp/artifacts
+curl bashupload.com -T $workdir/derp/out/target/product/earth/*.zip
+curl bashupload.com -T $workdir/derp/out/target/product/earth/*.zip.sha256sum
+curl bashupload.com -T $workdir/derp/out/target/product/earth/boot.img
+mv $workdir/derp/out/target/product/earth/*.zip /tmp/artifacts/.
+mv $workdir/derp/out/target/product/earth/*.zip.sha256sum /tmp/artifacts/.
+mv $workdir/derp/out/target/product/earth/boot.img /tmp/artifacts/.
